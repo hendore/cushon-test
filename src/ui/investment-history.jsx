b61b750e9a4cf402768c4a675d1f7bced409042b
Hendore <thenley28@googlemail.com>
2023-09-20 01:57:51 +0100

    Show message in investment history if no investments have been made yet

--- src/ui/investment-history.jsx
+++ src/ui/investment-history.jsx
@@ -6,6 +6,10 @@ import InvestmentHistoryItem from "./investment-history-item";
 export default function InvestmentHistory(props) {
   const investments = useSelector(selectInvestments);
 
+  if (investments.length == 0) {
+    return <p>You haven't made any investments yet.</p>;
+  }
+
   return investments.map((investment) => (
     <InvestmentHistoryItem key={investment.id} investment={investment} />
   ));
